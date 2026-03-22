4dbd81bff48da5ac09d0b7c39a41fa41e9829a13
leak <not@gmail.com>
2026-03-22 18:43:52 +0100

    fix: Remove encryption from server messages - client doesn't decrypt incoming packets

--- Protocol/Messages/Server/EndTurnMessage.js
+++ Protocol/Messages/Server/EndTurnMessage.js
@@ -252,27 +252,10 @@ class EndTurnMessage extends PiranhaMessage {
     this.writeInt(0)
   }
 
-  async send() {
-    try {
-      this.encode()
-
-      const id = Buffer.alloc(2)
-      id.writeUInt16BE(this.id, 0)
-
-      const len = Buffer.alloc(3)
-      len.writeUIntBE(this.buffer.length, 0, 3)
-
-      const message = Buffer.concat([id, len, this.buffer])
-
-      // Encrypt
-      const encrypted = await this.client.crypto.encrypt(message.slice(5))
-      const finalMessage = Buffer.concat([message.slice(0, 5), encrypted])
-
-      this.client.write(finalMessage)
-      this.client.log(`[EndTurnMessage] Sent end turn packet (${this.commands.length} commands)`)
-    } catch (err) {
-      console.error('Error sending EndTurnMessage:', err)
-    }
+  send() {
+    // Use base class send() - DO NOT encrypt server->client messages
+    // Client does not decrypt incoming messages (only client->server are encrypted)
+    super.send()
   }
 }
 
